4de8245ca9f9264cb441983cbbf40f30f60f817f
Jackfm54 <jackgomez72@gmail.com>
2025-02-22 16:26:29 -0500

    update image home

--- front/health-surveillance/src/pages/Home.jsx
+++ front/health-surveillance/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/Home.css';
+import homeImage from '../assets/images/home-img.jpg';
 
 const Home = () => {
   return (
     <div className="home">
       <h2>Bienvenue sur Surveillance de Santé</h2>
       <p>Surveillez votre santé à tout moment, où que vous soyez.</p>
+      <img src={homeImage} alt="Health monitoring" />
     </div>
   );
 };
